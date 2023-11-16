9dafa49f669bccaf4edbb2f2562306c4bd19c774
priyanka <priyankadeore91@gmail.com>
2023-11-16 13:22:51 +0000

    Merge branch 'master' of https://github.com/shubhramishra16/UnderstandFork

--- Fork/src/undersatandGitCommands/ToUnderstandFork.java
+++ Fork/src/undersatandGitCommands/ToUnderstandFork.java
@@ -4,6 +4,13 @@ public class ToUnderstandFork {
 
 	public static void main(String[] args) {
 		System.out.println("send the pull request");
+		System.out.println("send the pull request");
+		System.out.println("send the pull request");
+		System.out.println("send the pull request");
+		System.out.println("send the pull request");
+		System.out.println("send the pull request");
+		
+		
 
 	}
 
