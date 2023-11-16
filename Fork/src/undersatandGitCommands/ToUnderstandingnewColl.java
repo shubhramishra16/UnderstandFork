4657491cb00f8e7bcbb47a28c62dd418ca438e18
priyanka <priyankadeore91@gmail.com>
2023-11-16 12:26:42 +0000

    my new commit

--- Fork/src/undersatandGitCommands/ToUnderstandingnewColl.java
+++ Fork/src/undersatandGitCommands/ToUnderstandingnewColl.java
@@ -1,12 +1,11 @@
 package undersatandGitCommands;
 
-public class ToUnderstandTheCollbration {
+public class ToUnderstandingnewColl {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("");
-		
+		system.out.println("");
 	}
 
 }
