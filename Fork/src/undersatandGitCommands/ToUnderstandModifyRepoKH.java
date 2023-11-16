5cfdfe584bd9defc079ed82fbd333a05d53fe17d
jaga_ <jaga_@DESKTOP-UEHJSMH>
2023-11-16 12:13:58 +0000

    Added file ToUnderstandModifyRepoKH

---
+++ Fork/src/undersatandGitCommands/ToUnderstandModifyRepoKH.java
@@ -0,0 +1,9 @@
+package undersatandGitCommands;
+
+public class ToUnderstandModifyRepoKH {
+
+	public static void main(String[] args) {
+		System.out.println("Added this file to understand Modify without access");
+	}
+
+}
