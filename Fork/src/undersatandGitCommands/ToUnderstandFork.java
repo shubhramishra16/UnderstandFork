78006c791f64ab5c1afc91db447885f43f03005d
jaga_ <jaga_@DESKTOP-UEHJSMH>
2023-11-16 13:24:40 +0000

    Added 3 lines from line num 7 to 9

--- Fork/src/undersatandGitCommands/ToUnderstandFork.java
+++ Fork/src/undersatandGitCommands/ToUnderstandFork.java
@@ -4,7 +4,9 @@ public class ToUnderstandFork {
 
 	public static void main(String[] args) {
 		System.out.println("send the pull request");
-
+		System.out.println("Editing file same 1");
+		System.out.println("Editing file same 2");
+		System.out.println("Editing file same 3");		
 	}
 
 }
